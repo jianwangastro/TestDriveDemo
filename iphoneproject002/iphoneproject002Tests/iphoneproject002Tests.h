7701346904f17d78d6197b1c1d4adb66c22c5d30
Monkeys! <Monkeys@Yiqiu-Wangs-iMac.local>
2011-06-19 00:57:36 -0700

    adding test files.

---
+++ iphoneproject002/iphoneproject002Tests/iphoneproject002Tests.h
@@ -0,0 +1,17 @@
+//
+//  iphoneproject002Tests.h
+//  iphoneproject002Tests
+//
+//  Created by Monkeys! on 6/19/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface iphoneproject002Tests : SenTestCase {
+@private
+    
+}
+
+@end
---
+++ iphoneproject002/iphoneproject002Tests/iphoneproject002Tests.m
@@ -0,0 +1,33 @@
+//
+//  iphoneproject002Tests.m
+//  iphoneproject002Tests
+//
+//  Created by Monkeys! on 6/19/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "iphoneproject002Tests.h"
+
+
+@implementation iphoneproject002Tests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in iphoneproject002Tests");
+}
+
+@end
