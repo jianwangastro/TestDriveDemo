3f9f1a1c37a831c239b763711e7e9e7aed6f602f
Jian Wang <jason.wang.cs@gmail.com>
2011-07-02 23:53:18 -0700

    adding new files.

---
+++ aaa/aaaTests/aaaTests.h
@@ -0,0 +1,17 @@
+//
+//  aaaTests.h
+//  aaaTests
+//
+//  Created by Wang, Jian on 6/18/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface aaaTests : SenTestCase {
+@private
+    
+}
+
+@end
---
+++ aaa/aaaTests/aaaTests.m
@@ -0,0 +1,33 @@
+//
+//  aaaTests.m
+//  aaaTests
+//
+//  Created by Wang, Jian on 6/18/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "aaaTests.h"
+
+
+@implementation aaaTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in aaaTests");
+}
+
+@end
