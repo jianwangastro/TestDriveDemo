c23f5390c90796894912089a7e8f95279cd8da7b
Monkeys! <Monkeys@Yiqiu-Wangs-iMac.local>
2011-07-04 13:17:45 -0700

    Adding setting view.

---
+++ iphoneproject002/iphoneproject002/secondView.h
@@ -0,0 +1,18 @@
+//
+//  secondView.h
+//  
+//
+//  Created by Monkeys! on 7/4/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface secondView : UIViewController {
+    NSArray *contentArray;
+}
+
+-(IBAction)switchBack:(id)sender;
+
+@end
---
+++ iphoneproject002/iphoneproject002/settings.h
@@ -0,0 +1,17 @@
+//
+//  settings.h
+//  
+//
+//  Created by Monkeys! on 7/4/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface settings : UIViewController {
+    
+}
+-(IBAction)switchBack:(id)sender;
+
+@end
